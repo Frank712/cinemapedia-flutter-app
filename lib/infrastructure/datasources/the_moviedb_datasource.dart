1ca989518538479c22d29702074acf4b135e11a6
Francisco Lino <franksuriel_7@hotmail.com>
2023-04-17 15:26:32 -0600

    ADD using my first provider

--- lib/infrastructure/datasources/the_moviedb_datasource.dart
+++ lib/infrastructure/datasources/the_moviedb_datasource.dart
@@ -17,7 +17,7 @@ class MovieDBDataSource extends MoviesDataSource {
   );
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response = await dio.get('/movies/now_playing');
+    final response = await dio.get('/movie/now_playing');
     final movieDbResponse = MovieDbResponse.fromJson(response.data);
     final List<Movie> movies = movieDbResponse.results
         .where((movieDb) => movieDb.posterPath != 'no-poster')
