3d64455cbe408e0f39634b5d49ca037139bef289
Francisco Lino <franksuriel_7@hotmail.com>
2023-05-25 00:21:56 -0600

    ADD data sources for MovieDetails

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,25 +1,47 @@
 import 'package:cinemapedia_flutter_app/domain/entities/movie.dart';
+import 'package:cinemapedia_flutter_app/infrastructure/models/moviesdb/movie_details.dart';
 import 'package:cinemapedia_flutter_app/infrastructure/models/moviesdb/movie_moviedb.dart';
 
 class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
-      adult: moviedb.adult,
-      backdropPath: (moviedb.backdropPath != '') 
-        ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
-        : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png',
-      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
-      id: moviedb.id,
-      originalLanguage: moviedb.originalLanguage,
-      originalTitle: moviedb.originalTitle,
-      overview: moviedb.overview,
-      popularity: moviedb.popularity,
-      posterPath: (moviedb.posterPath != '') 
-        ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-        : 'no-poster',
-      releaseDate: moviedb.releaseDate,
-      title: moviedb.title,
-      video: moviedb.video,
-      voteAverage: moviedb.voteAverage,
-      voteCount: moviedb.voteCount,
+        adult: moviedb.adult,
+        backdropPath: (moviedb.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+            : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png',
+        genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+        id: moviedb.id,
+        originalLanguage: moviedb.originalLanguage,
+        originalTitle: moviedb.originalTitle,
+        overview: moviedb.overview,
+        popularity: moviedb.popularity,
+        posterPath: (moviedb.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+            : 'no-poster',
+        releaseDate: moviedb.releaseDate,
+        title: moviedb.title,
+        video: moviedb.video,
+        voteAverage: moviedb.voteAverage,
+        voteCount: moviedb.voteCount,
+      );
+
+  static Movie movieDetailsToEntity(MovieDetails movie) => Movie(
+        adult: movie.adult,
+        backdropPath: (movie.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${movie.backdropPath}'
+            : 'https://sd.keepcalms.com/i/keep-calm-poster-not-found.png',
+        genreIds: movie.genres.map((e) => e.name).toList(),
+        id: movie.id,
+        originalLanguage: movie.originalLanguage,
+        originalTitle: movie.originalTitle,
+        overview: movie.overview,
+        popularity: movie.popularity,
+        posterPath: (movie.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
+            : 'no-poster',
+        releaseDate: movie.releaseDate,
+        title: movie.title,
+        video: movie.video,
+        voteAverage: movie.voteAverage,
+        voteCount: movie.voteCount,
       );
 }
