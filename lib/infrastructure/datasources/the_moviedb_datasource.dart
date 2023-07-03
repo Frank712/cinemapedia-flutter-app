a82e263d57a43b446df38d442040f47e06b53739
Francisco Lino <franksuriel_7@hotmail.com>
2023-07-02 22:54:41 -0600

    ADD repository and data source to search

--- lib/infrastructure/datasources/the_moviedb_datasource.dart
+++ lib/infrastructure/datasources/the_moviedb_datasource.dart
@@ -64,4 +64,11 @@ class MovieDBDataSource extends MoviesDataSource {
     final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
     return movie;
   }
+  
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+    return _jsonToMovies(response.data);
+  }
 }
